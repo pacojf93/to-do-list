31415f4bc20e14f7b8e7292ce8b20b2041caec47
Paco <127772244+pacojf93@users.noreply.github.com>
2025-01-19 12:25:08 +0100

    refactored render methods

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+//import storage from './modules/storage'
 import './styles.css'
 
 const projectList = (function() {
@@ -34,6 +35,11 @@ function project (title) {
 function toDo (title, description, dueDate, priority) {
     const notes = []
     const checklist = []
+    const priorityValues = {
+        LOW: "LOW",
+        MEDIUM: "MEDIUM",
+        HIGHG: "HIGH"
+    }
 
     const addNote = (content) => {
         const id = 0
@@ -50,7 +56,8 @@ function toDo (title, description, dueDate, priority) {
         notes,
         checklist,
         addNote,
-        delNote
+        delNote,
+        priorityValues
     }
 }
 
@@ -65,39 +72,10 @@ function delChild (father) {
     return (index) => father.splice(index,1) 
 }
 
-const render = (function(doc){
+const render = (function(doc,pList){
     const projectContainer = doc.getElementById("projects-list")
     const toDoContainer = doc.getElementById("to-do-list")
     const notesContainer = doc.getElementById("notes-list")
-
-    const renderProjects = () => {
-        projectList.projects.map(
-            (project,index) => {
-                const div = document.createElement("div")
-                div.id = `project-${index}`
-                div.textContent = project.title
-                return div
-        }).forEach(div => projectContainer.appendChild(div))
-    }  
-
-    const renderToDo = (index) => {
-        projectList.projects[index].toDos.map((toDo, toDoIndex) => {
-            const div = document.createElement("div")
-            div.id = `todo-${index}-${toDoIndex}`
-            div.textContent = toDo.title
-            return div
-        }).forEach(div => toDoContainer
-            .appendChild(div))
-    }
-
-    const renderNotes = (projecIndex, toDoIndex) => {
-        projectList.projects[projecIndex].toDos[toDoIndex].notes.map((note, noteIndex) => {
-            const div = document.createElement("div")
-            div.id = `note-${projecIndex}-${toDoIndex}-${noteIndex}`
-            div.textContent = note.content
-            return div
-        }).forEach(div => notesContainer.appendChild(div))
-    }
 
     const emptyContainer = (container) => () => Array
     .from(container.querySelectorAll("*"))
@@ -106,41 +84,145 @@ const render = (function(doc){
     const emptyToDo = emptyContainer(toDoContainer)
     const emptyNotes = emptyContainer(notesContainer)
 
-    projectContainer.addEventListener('click', (e) => {
-        let index = e.target.id.replace('project-','')
+    const createElementWithContentAndId = (selector) => (text,id) =>{
+        const element = document.createElement(selector)
+        element.textContent = text
+        element.id = id
+        return element
+    }
+    const createButton = createElementWithContentAndId("button")
+    const createDiv = createElementWithContentAndId("div")
+    const contentAndDelButton = (content,id) => {
+        const div = createDiv('', '')
+        const contentDiv = createDiv(content, `show-${id}`)
+        const delButton = createButton('del', `del-${id}`)
+
+        div.appendChild(contentDiv)
+        div.appendChild(delButton) 
+
+        return div
+    }
+    const contentDelButtonAndAddButton = (content, id) => {
+        const div = contentAndDelButton(content, id)
+        const addButton = createButton('add', `add-${id}`)
+        div.appendChild(addButton)
+        return div
+    }
+
+    const renderProjects = () => {
+        emptyProjects()
+        pList.projects.map(
+            (project,index) => contentDelButtonAndAddButton(project.title, `project-${index}`)
+        ).forEach(div => projectContainer.appendChild(div))
+    }  
+
+    const renderToDo = (index) => {
         emptyToDo()
+        pList.projects[index].toDos.map(
+            (toDo, toDoIndex) => contentDelButtonAndAddButton(toDo.title, `todo-${index}-${toDoIndex}`)
+        ).forEach(div => toDoContainer.appendChild(div))
+    }
+
+    const renderNotes = (projecIndex, toDoIndex) => {
+        emptyNotes()
+        pList.projects[projecIndex].toDos[toDoIndex].notes.map(
+            (note, noteIndex) => contentAndDelButton(note.content, `note-${projecIndex}-${toDoIndex}-${noteIndex}`)
+        ).forEach(div => notesContainer.appendChild(div))
+    }
+
+    const showToDosInProject = (index) => {
         renderToDo(index)
+    }
+
+    const delProject = (index) => {
+        pList.delProject(index)
+        renderProjects()
+    }
+
+    const addToDo = (index) => {
+        pList.projects[index].addToDo(
+            'this is a new to do',
+            'this is the description of the new to do',
+            0,
+            0
+        )
+        showToDosInProject(index)
+    }
+
+    const showNotesInToDo = (index) => {
+        renderNotes(index[0],index[1])
+    }
+
+    const delToDo = (index) => {
+        pList.projects[index[0]].delToDo(index[1])
+        renderToDo(index[0])
+    }
+
+    const addNote = (index) => {
+        pList.projects[index[0]].toDos[index[1]].addNote(
+            'this is the content of a new note',
+        )
+        showNotesInToDo(index)
+    }    
+
+    const delNote = (index) => {
+        pList.projects[index[0]].toDos[index[1]].delNote(index[2])
+        renderNotes(index[0],index[1])
+    }
+
+    const clickHandlerWithMethods = (methods) => (e) => {
+        let key = e.target.id.match(/\w+/)[0]
+        let index = e.target.id.match(/\d+/g)
+        console.log('',key,index)
+        if(methods.hasOwnProperty(key)) methods[key](index)         
+    }
+
+    const projectClickHandler = clickHandlerWithMethods({
+        show: showToDosInProject,
+        del: delProject,
+        add: addToDo
     })
+
+    const toDoClickHandler = clickHandlerWithMethods({
+        show: showNotesInToDo,
+        del: delToDo,
+        add: addNote
+    })
+
+    const notesClickHandler = clickHandlerWithMethods({
+        del: delNote
+    })
+
+    projectContainer.addEventListener('click', projectClickHandler)
+    toDoContainer.addEventListener('click', toDoClickHandler)
+    notesContainer.addEventListener('click', notesClickHandler)    
     
     return {
         renderProjects,
         renderToDo,
         renderNotes,
-        emptyProjects,
-        emptyToDo,
-        emptyNotes
     }
     
-})(document)
+})(document,projectList)
 
-const storage = (function (){
-    const saveProjects = () => localStorage.projectList = JSON.stringify(projectList)
+const storage = (function (pList){
+    const saveProjects = () => localStorage.projectList = JSON.stringify(pList)
     const restorePorjects = () => {
         const savedProjects = JSON.parse(localStorage.projectList)
         
         savedProjects.projects.forEach((project, projectIndex) => {
-            projectList.addProject(project.title)
+            pList.addProject(project.title)
             
             project.toDos.forEach((toDo, toDoIndex) => {
-                projectList.projects[projectIndex].addToDo(
+                pList.projects[projectIndex].addToDo(
                     toDo.title,
                     toDo.description,
                     toDo.dueDate,
                     toDo.priority
                 )
 
-                toDo.notes.forEach((note, noteIndex) => {
-                    projectList.projects[projectIndex].toDos[toDoIndex].addNote(note.content)
+                toDo.notes.forEach((note) => {
+                    pList.projects[projectIndex].toDos[toDoIndex].addNote(note.content)
                 })
 
             })
@@ -151,7 +233,7 @@ const storage = (function (){
         saveProjects,
         restorePorjects
     }
-})()
+})(projectList)
 
 /* for(let i = 0; i < 10; i++) {
     projectList.addProject(`project number ${i}`)
